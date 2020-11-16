9d3602853a589130fd1ed3756c2d576a87cb644a
yingyangle <cyang1999@gmail.com>
2020-11-15 20:15:59 -0500

    upload data

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const m = 50
+const margin = ({ top: m, right: m, bottom: m, left: m })
+const width = 1100 - margin.left - margin.right
+const height = 600 - margin.top - margin.bottom
+
+d3.csv('driving.csv', d3.autoType).then(data => {
+
+
+});
